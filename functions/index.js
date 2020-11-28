2f1f6c4fa6da7dc24a87492c5dc4c1ca3e1c6499
Magdalena Lipka <magdalenalipka@int.pl>
2020-11-28 06:33:39 +0100

    update retrieving data from request body

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,12 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const functions = require("firebase-functions");
-const { loginUser, getUser, updateUser, getUserChallenges } = require("./users");
+const {
+  loginUser,
+  getUser,
+  updateUser,
+  getUserChallenges,
+} = require("./users");
 const { getChallenge, createChallenge, addGroup } = require("./challenges");
 const { getGroup, createGroup, addMember } = require("./groups");
 
@@ -17,7 +22,7 @@ app.get("/", (_, res) => res.send("Hello!"));
 
 // ---------- Users
 app.post("/hello", async (req, res) => {
-  const { mail } = JSON.parse(req.body);
+  const { mail } = req.body;
   res.status(200).send({ id: await loginUser(mail) });
 });
 
@@ -34,7 +39,7 @@ app.get("/users/:id/approaches", async (req, res) => {
 });
 
 app.patch("/users/:id", async (req, res) => {
-  const { ...user } = JSON.parse(req.body);
+  const { ...user } = req.body;
   res.send(await updateUser(user));
 });
 
@@ -48,12 +53,12 @@ app.get("/challenges", async (req, res) => {
 });
 
 app.post("/challenges", async (req, res) => {
-  const { ...challenge } = JSON.parse(req.body);
+  const { ...challenge } = req.body;
   res.send(await createChallenge(challenge));
 });
 
 app.post("/challenges/:id/groups", async (req, res) => {
-  const { group_id } = JSON.parse(req.body);
+  const { group_id } = req.body;
   res.send(await addGroup({ challenge_id: id, group_id }));
 });
 
@@ -63,12 +68,12 @@ app.get("/groups/:id", async (req, res) => {
 });
 
 app.post("/groups", async (req, res) => {
-  const { ...group } = JSON.parse(req.body);
+  const { ...group } = req.body;
   res.send(await createGroup(group));
 });
 
 app.post("/groups/:id/members", async (req, res) => {
-  const { user_id } = JSON.parse(req.body);
+  const { user_id } = req.body;
   res.send(await addMember({ group_id: id, user_id }));
 });
 
@@ -78,18 +83,18 @@ app.get("/approach/:id", async (req, res) => {
 });
 
 app.post("/approach", async (req, res) => {
-  const { ...approach } = JSON.parse(req.body);
+  const { ...approach } = req.body;
   res.send(await createApproach(approach));
 });
 
 app.post("/approach/:id/location", async (req, res) => {
-  const { ...location } = JSON.parse(req.body);
-  res.send(await addLocation({id, location}));
+  const { ...location } = req.body;
+  res.send(await addLocation({ id, location }));
 });
 
 app.post("/approach/:id/verify", async (req, res) => {
-  const { photo } = JSON.parse(req.body);
-  await verifyPhoto({id, photo})
+  const { photo } = req.body;
+  await verifyPhoto({ id, photo });
 
   res.send("Photo accepted to verification.");
 });
