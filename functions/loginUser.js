cd7c687c97dfb1a3720a05ac795eba10b538b5c8
Magdalena Lipka <magdalenalipka@int.pl>
2020-11-28 01:04:31 +0100

    finish logging users

--- functions/loginUser.js
+++ functions/loginUser.js
@@ -5,25 +5,21 @@ const firestore = new Firestore();
 const usersRef = firestore.collection("users");
 
 const loginUser = async (mail) => {
-  const users = [];
 
   const snapshot = await usersRef.where("mail", "==", mail).get();
 
   if (snapshot.empty) {
-    console.log("No matching documents.");
-    return;
+    const id = await createUser(mail)
+    return id;
   }
 
-  snapshot.forEach((doc) => {
-    users.push({ id: doc.id, data: doc.data() });
-  });
-
-  return users;
+  return snapshot[0].id;
 };
 
 const createUser = async (mail) => {
   const id = v4();
   await usersRef.doc(id).set({ mail });
+  return id;
 };
 
 module.exports = {
